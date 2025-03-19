4a700dcdf4b8bd7db28be7cef7d459bc6155bed4
Namefix <41016273+Namefix@users.noreply.github.com>
2025-03-19 03:24:04 +0300

    website update

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let textBackground = document.querySelector(".coloredtxt");
 let wobble = document.querySelectorAll(`#wobble`);
 let stuffText = document.querySelector(`#stuff`);
 let stuffStreakText = document.querySelector(`#stuffstreak`);
+let discord = document.querySelector(`#discord`);
+let secretSpace = document.querySelector(`.secret`);
 
 let posX = 300;
 let posY = 300;
@@ -14,21 +16,21 @@ let stuffIndex = 0;
 let stuffStreak = 0;
 let stuffList = [
 	"stuff I made",
+	"stuff I did",
 	"stuff I do",
-	"stuff I try to do?",
-	"stuff I'll keep doing?",
-	"stuff I <i>maybe</i> do?",
+	"stuff I'll keep doing",
+	"stuff I'll not stop doing",
 	"stuff I hopefully do more of",
-	"stuff I probably do",
+	"stuff I hopefully not do less of",
 	"stuff I like doing",
 	"stuff I don't hate doing",
-	"it looks like I've ran out of things to add here",
-	"well it's nice knowing ya, back to the first line"
+	"looks like I've ran out of things to add here",
+	"thank you, back to the first line"
 ]
 
 function animate() {
-	posX += 0.1;
-	posY += 0.1;
+	posX += 0.2;
+	posY += 0.2;
 	textBackground.style = `background-position: ${posX}px ${posY}px;`;
 
 	animateWobble();
@@ -53,5 +55,26 @@ stuffText.addEventListener("click", () => {
 		stuffStreakText.innerHTML = "streak x"+stuffStreak;
 	}
 	stuffText.innerHTML = stuffList[stuffIndex];
+	secretSpace.innerHTML = "secret for a secret"
+})
+
+discord.addEventListener("click", () => {
+	alert("just namefix");
+})
+
+secretSpace.addEventListener("click", () => {
+	alert("don't touch me!")
+})
+
+textBackground.addEventListener("mouseover", () => {
+	document.body.classList.add("bg-active");
+	textBackground.classList.add("coloredtxt-active");
+	secretSpace.classList.add("begone");
+})
+
+textBackground.addEventListener("mouseout", () => {
+	document.body.classList.remove("bg-active");
+	textBackground.classList.remove("coloredtxt-active");
+	secretSpace.classList.remove("begone");
 })
 
